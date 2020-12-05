c5ea658d39473508c7017c200f9d36bd0322ac26
FacundoOs <osores.facundo@gmail.com>
2020-12-05 21:00:34 +0100

    create profile and user models and profiles and users routes

---
+++ models/profile.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Exercise = mongoose.model("Profile", profileSchema);
+
+module.exports = Exercise;
--- server.js
+++ server.js
@@ -22,6 +22,13 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
+const profilesRouter = require("./routes/profiles");
+const usersRouter = require("./routes/users");
+
+app.use("/profiles", profilesRouter);
+app.use("/users", usersRouter);
+
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
