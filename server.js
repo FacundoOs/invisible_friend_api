8341a484095392a07a7c1c27dfa17c635a6ef885
FacundoOs <osores.facundo@gmail.com>
2020-12-07 14:01:53 +0100

    test heroku

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => { res.send('Hello from Express!')})
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
